533fc01bf3a1908a0f9ca8d7375656022efd3553
Tu Ngoc Van <tungocvan@gmail.com>
2023-04-27 21:35:12 +0700

    make controller and model

--- app/Console/Commands/CreateAndMoveControllerCommand.php
+++ app/Console/Commands/CreateAndMoveControllerCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands; 
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
@@ -8,30 +8,37 @@ use Illuminate\Support\Facades\File;
 
 class CreateAndMoveControllerCommand extends Command
 {
-    protected $signature = 'create:controller {controller} {directory}';
+    protected $signature = 'create:controller {controller} {directoryModule} {--resource}';
 
     protected $description = 'Create a controller file and move it to a specified directory';
 
     public function handle()
     {
-        $controllerName = $this->argument('controller');
-        $directory = $this->argument('directory');
-
+        $controllerName  = $this->argument('controller')."Controller";
+        $nameModule = $this->argument('directoryModule');
+        $directory = "modules/{$nameModule }/src/Http/Controllers"; 
         $controllerPath = app_path('Http/Controllers/' . $controllerName . '.php');
         $newControllerPath = $directory . '/' . $controllerName . '.php';
-
+        $myoption = $this->option('resource');
+        //dd($myoption);
         if (File::exists($newControllerPath)) {
             $this->error('Controller file already exists in directory');
             return;
         }
 
-        Artisan::call('make:controller', ['name' => $controllerName]);
+        if($myoption){
+            Artisan::call('make:controller', ['name' => $controllerName,'--resource' => $myoption]);
+        }else{
+            Artisan::call('make:controller', ['name' => $controllerName]);
+        }
+        
         File::move($controllerPath, $newControllerPath);
 
         // Đọc nội dung file model
         $content = file_get_contents($newControllerPath);
         $newNamespace = str_replace('/', '\\', $directory);
         $content = str_replace('namespace App\Http\Controllers;', "namespace {$newNamespace};", $content);
+        $content .= "\n use Illuminate\Http\Request;";
         file_put_contents($newControllerPath, $content);
 
         $this->info('Controller file created and moved successfully');
--- app/Console/Commands/CreateAndMoveModel.php
+++ app/Console/Commands/CreateAndMoveModel.php
@@ -13,7 +13,9 @@ class CreateAndMoveModel extends Command
     public function handle()
     {
         $name = $this->argument('name');
-        $path = $this->argument('path');
+        $nameModule = $this->argument('path');
+        $path = "modules/{$nameModule}/src/Models";
+        //dd($path);
         if (!is_dir(realpath($path))) {
             // đường dẫn không tồn tại hoặc không phải là một thư mục
             // xử lý lỗi tại đây
@@ -46,6 +48,18 @@ class CreateAndMoveModel extends Command
         // Thay đổi namespace của model bằng đường dẫn mới
         $newNamespace = str_replace('/', '\\', $path);
         $content = str_replace('namespace App\Models;', "namespace {$newNamespace};", $content);
+        //$table = 'protected $table = '';';
+        //$content = str_replace('use HasFactory;', "use HasFactory;\n {$table}", $content);
+        file_put_contents($newPath, $content);
+        $content = file_get_contents($newPath);
+        $table =  '  //protected $table = "";';
+        $table .="\n".  '   //protected $primaryKey = "id";';
+        $table .="\n".  '   //protected $fillable = [];';
+        $table .="\n".  '   //protected $timestamps = true;';
+        $table .="\n".  '   //const CREATED_AT ="created_at";';
+        $table .="\n".  '   //const UPDATED_AT ="updated_at";';        
+        
+        $content = str_replace('use HasFactory;', "use HasFactory;\n {$table}", $content);
         file_put_contents($newPath, $content);
 
         $this->info("Model {$name} created and moved to {$newPath}");
